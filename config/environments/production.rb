f42dbf763cc25a02ce3edca3b8fd44e30076227d
Trevor Gauntlett <Trevor@gauntlettcreative.com>
2016-03-17 17:36:44 -0700

    forgot to save file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,14 @@ Rails.application.configure do
 
   #devise config as per installation instructions ~> heroku app hosting
   config.action_mailer.default_url_options = { host: 'rails-pint-tutorial.herokuapp.com' }
+
+  # config for paperclip with AWS -- using AWS SDK v1
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
 end
